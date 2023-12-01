7c076a430d91fd68ca440ffc12a6f7bab323a51f
devNovachrono <94188088+devNovachrono@users.noreply.github.com>
2023-12-01 02:44:51 -0500

    Day 1

---
+++ main.js
@@ -0,0 +1,49 @@
+import { calDoc } from './calibrationDoc.js';
+
+let digitNames = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine"
+]
+
+let calDocArr = calDoc.split("\n");
+
+function getDigits(str) {
+    var allMatches = [];
+    for (var i = 1; i < digitNames.length; i++)
+    {
+        let digitName = digitNames[i];
+        let charMatches = str.matchAll(new RegExp("(" + digitName + "|" + i + ")", 'g'));
+        for (var match of charMatches)
+        {
+            
+            allMatches[match["index"]] = i + "";
+        }
+    }
+    return allMatches.filter(function (e) {
+        return e != null;
+    });
+}
+
+function getCalculation()
+{
+    var result = 0;
+    calDocArr.forEach(element => {
+        let digits = getDigits(element);
+        let firstNum = digits[0];
+        let lastNum = digits[digits.length - 1];
+        console.log(element + " : " + firstNum + lastNum);
+        result += parseInt(firstNum + lastNum);
+    });
+    return result;
+}
+
+var calValue = getCalculation();
+console.log(calValue);
