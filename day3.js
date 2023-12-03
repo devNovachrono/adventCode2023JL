568c232962022879a30f7dad20e8744cf8583a11
devNovachrono <94188088+devNovachrono@users.noreply.github.com>
2023-12-03 02:00:01 -0500

    Day3: just a little bit of spaghetti on pt2

---
+++ day3.js
@@ -0,0 +1,73 @@
+import { engineSchematic } from './engineSchematic.js';
+
+function findValidParts(schem, gearRatios = false)
+{
+    var partNumbers = gearRatios ? {} : [];
+    let relations = [
+        1,
+        -1,
+        140,
+        -140,
+        -141,
+        -139,
+        139,
+        141
+    ];
+    let matches = schem.matchAll(new RegExp("[0-9]+", 'g'));
+    for (var match of matches)
+    {
+        let v = match[0];
+        let vIndex = match["index"];
+loop1:
+        for (var i = 0; i < v.length; i++)
+        {
+            for (var relation of relations)
+            {
+                let pos = i + vIndex + relation;
+                if (pos >= schem.length || pos < 0) continue;
+                let a = schem[i + vIndex + relation];
+                if (a == '.' || !isNaN(a)) continue;
+                if (gearRatios)
+                {
+                    if (a != "*") continue;
+                    if (partNumbers[pos] == null)
+                    {
+                        partNumbers[pos] = [v];
+                    }
+                    else
+                    {
+                        partNumbers[pos].push(v);
+                    }
+                }
+                else
+                {
+                    partNumbers.push(parseInt(v));
+                }
+                break loop1;
+            }
+        }
+    }
+    if (gearRatios)
+    {
+        let old = partNumbers;
+        partNumbers = {};
+        for (var a in old)
+        {
+            var b = old[a];
+            if (b.length > 1)
+            {
+                partNumbers[a] = b;
+            }
+        }
+    }
+    return partNumbers;
+}
+
+let schematic = engineSchematic.replace(/\r\n|\n|\r/gm, "");
+let isolatedSchem = findValidParts(schematic);
+let partNumberSum = (() => {let v = 0; for (let i in isolatedSchem) {v += isolatedSchem[i];} return v;})();
+
+let gearRatioSchem = findValidParts(schematic, true);
+let gearRatioSum = (() => {let v = 0; for (let i in gearRatioSchem) {v += gearRatioSchem[i][0] * gearRatioSchem[i][1];} return v;})();
+
+export const day3 = [partNumberSum, gearRatioSum];
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { day1 } from './day1.js';
 import { day2 } from './day2.js';
+import { day3 } from './day3.js';
 
 console.log("Day 1: " + day1);
 console.log("Day 2: " + day2[0] + " ; " + day2[1]);
+console.log("Day 3: " + day3[0] + " : " + day3[1]);
