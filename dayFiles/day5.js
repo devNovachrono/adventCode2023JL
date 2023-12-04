caf6ac1a8d33ad4e01cba6cfad485f4e61db6847
devNovachrono <94188088+devNovachrono@users.noreply.github.com>
2023-12-04 17:29:03 -0500

    day5 setup: trying new template

---
+++ dayFiles/day5.js
@@ -0,0 +1,17 @@
+import { input } from '../input/day5input.js';
+
+//shared functions
+
+//main
+function part1()
+{
+    return "";
+}
+
+function part2()
+{
+
+    return "";
+}
+
+export const day5 = [part1(), part2()];
--- main.js
+++ main.js
@@ -2,8 +2,10 @@ import { day1 } from './dayFiles/day1.js';
 import { day2 } from './dayFiles/day2.js';
 import { day3 } from './dayFiles/day3.js';
 import { day4 } from './dayFiles/day4.js';
+import { day5 } from './dayFiles/day5.js';
 
 console.log("Day 1: " + day1);
 console.log("Day 2: " + day2[0] + " ; " + day2[1]);
 console.log("Day 3: " + day3[0] + " ; " + day3[1]);
 console.log("Day 4: " + day4[0] + " ; " + day4[1]);
+console.log("Day 5: " + day5[0] + " ; " + day5[1]);
