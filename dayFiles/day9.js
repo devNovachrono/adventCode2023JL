169cca173d78072254acfae12ac638bc1f060233
devNovachrono <94188088+devNovachrono@users.noreply.github.com>
2023-12-09 01:46:45 -0500

    Update day9.js

--- dayFiles/day9.js
+++ dayFiles/day9.js
@@ -3,53 +3,25 @@ import { input } from "../input/day9input.js";
 
 let histories = input.split('\n').map(x => {return x.split(" ").map(y => {return parseInt(y);});});
 
-function getNextSequence(cs)
+function getNextSequence(cs) {return cs.map((x,y) => {if (y - 1 >= 0) return cs[y] - cs[y - 1];}).filter(x => {return x !== undefined;});}
+
+function getExpected(h, d)
 {
-    return cs.map((x,y) => {
-        if (y - 1 >= 0)
-        {
-            return cs[y] - cs[y - 1];
-        }
-    }).filter(x => {return x !== undefined;});
+    d = Math.min(Math.max(d, -1), 1);
+    if (d == 0) return h;
+    let sequences = [];
+    while(h.filter(x => {return x == h[0]}).toString() != h.toString())
+    {
+        sequences.push(h);
+        h = getNextSequence(h);
+    }
+    let seqSum = h[0];
+    sequences = d==1 ? sequences : sequences.reverse();
+    sequences.forEach(s => {seqSum = s[d==1?s.length - 1:0] + (seqSum * d);});
+    return seqSum;
 }
 
-function part1()
-{
-    let sum = 0;
-    histories.forEach(h => {
-        let sequences = [];
-        while(h.filter(x => {return x == h[0]}).toString() != h.toString())
-        {
-            sequences.push(h);
-            h = getNextSequence(h);
-        }
-        let seqSum = h[0];
-        sequences.forEach(s => {
-            seqSum += s[s.length - 1];
-        });
-        sum += seqSum;
-    });
-    return sum;
-}
-
-function part2()
-{
-    let sum = 0;
-    histories.forEach(h => {
-        let sequences = [];
-        while(h.filter(x => {return x == h[0]}).toString() != h.toString())
-        {
-            sequences.push(h);
-            h = getNextSequence(h);
-        }
-        let seqSum = h[0];
-        sequences.reverse().forEach(s => {
-            seqSum = s[0] - seqSum;
-        });
-        sum += seqSum;
-    });
-    console.log(sum);
-    return sum;
-}
+function part1(){return histories.map(h => {return getExpected(h,1);}).reduce((a,b) => a+b, 0);}
+function part2(){return histories.map(h => {return getExpected(h,-1);}).reduce((a,b) => a+b, 0);}
 
 export const day9 = [part1(),part2()];
